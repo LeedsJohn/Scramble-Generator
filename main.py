411d7a31840d7130172a9ffa944a89909cfa8f7b
John Leeds <leedsjohn6@gmail.com>
2022-05-26 13:40:57 -0700

    Added a main function

--- main.py
+++ main.py
@@ -3,17 +3,12 @@ import Mover
 import StateGenerator
 import ScrambleGenerator as scrambler
 
-# john = StateGenerator.StateGenerator()
-# john.writeStates()
+def main():
+    state_gen = StateGenerator.StateGenerator()
+    state_gen.writeStates()
 
-john = scrambler.ScrambleGenerator()
-john.writeScrambles()
+    scram_gen = scrambler.ScrambleGenerator()
+    scram_gen.writeScrambles()
 
-
-# solved = 'UUUUUUUUURRRRRRRRRFFFFFFFFFDDDDDDDDDLLLLLLLLLBBBBBBBBB'
-# scramble = "F' L' U F2 R' U2 F R' L2 D2 L2 F2 U2 B D2 B U2 R2 F L2"
-# john = Mover.Mover(solved)
-# john.scramble(scramble)
-# print(f"\n\nREVERSED: {john.reverse(scramble)}\n")
-# john.scramble(john.reverse(scramble))
-# print(john)
+if __name__ == "__main__":
+    main()
