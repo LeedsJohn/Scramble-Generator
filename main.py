6a56740c0977eecab6145ed4f0c7dd4a4bfc6e9c
John Leeds <leedsjohn6@gmail.com>
2022-06-17 10:11:09 -0500

    Edited to go through multiple algsets

--- main.py
+++ main.py
@@ -2,12 +2,20 @@ import twophase.solver as sv
 import StateGenerator
 import ScrambleGenerator as scrambler
 
-def main():
-    state_gen = StateGenerator.StateGenerator("WV.txt", "WV_states.json", "PLL")
-    state_gen.writeStates()
 
-    scram_gen = scrambler.ScrambleGenerator("WV_states.json", "WV_scrambles.json")
-    scram_gen.writeScrambles()
+def main():
+    algSets = {"CMLL": "L4E", "COLL": "L4E", "ELL": "solved", "OLL": "PLL", "PLL": "solved", "WVLS_FL": "PLL", "WVLS": "PLL", "ZBLL_AS": "solved",
+               "ZBLL_H": "solved", "ZBLL_L": "solved", "ZBLL_Pi": "solved", "ZBLL_S": "solved", "ZBLL_T": "solved", "ZBLL_U": "solved"}
+    for algSet in algSets:
+        print(algSet)
+        state_gen = StateGenerator.StateGenerator(
+            f"input/{algSet}.txt", f"states/{algSet}.json", algSets[algSet])
+        state_gen.writeStates()
+
+        scram_gen = scrambler.ScrambleGenerator(
+            f"states/{algSet}.json", f"scrambles/{algSet}.json")
+        scram_gen.writeScrambles()
+
 
 if __name__ == "__main__":
     main()
