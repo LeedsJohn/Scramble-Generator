0e74955961f7fa053cfea53d04bfd3337809b8ad
John Leeds <leedsjohn6@gmail.com>
2022-07-05 15:30:19 -0500

    Rewrote to include 2x2 scrambles

--- main.py
+++ main.py
@@ -1,20 +1,29 @@
-import twophase.solver as sv
 import state_generators.ThreeStates as ThreeStates
-import scramble_generators.ThreeScrambles as scrambler
+import state_generators.TwoStates as TwoStates
+import scramble_generators.ThreeScrambles as ThreeScrambles
+import scramble_generators.TwoScrambles as TwoScrambles
 
 
 def main():
-    # algSets = {"PLL": "solved"}
-    algSets = {"CMLL": "L4E", "COLL": "L4E", "ELL": "solved", "OLL": "PLL", "PLL": "solved", "WVLS_FL": "PLL", "WVLS": "PLL", "ZBLL_AS": "solved",
-               "ZBLL_H": "solved", "ZBLL_L": "solved", "ZBLL_Pi": "solved", "ZBLL_S": "solved", "ZBLL_T": "solved", "ZBLL_U": "solved"}
-    for algSet in algSets:
+    three = {}
+    # three = {"CMLL": "L4E", "COLL": "L4E", "ELL": "solved", "OLL": "PLL", "PLL": "solved", "WVLS_FL": "PLL", "WVLS": "PLL", "ZBLL_AS": "solved",
+    #            "ZBLL_H": "solved", "ZBLL_L": "solved", "ZBLL_Pi": "solved", "ZBLL_S": "solved", "ZBLL_T": "solved", "ZBLL_U": "solved"}
+    # two = ["bruh"]
+    two = ["CLL", "EG-1", "EG-2", "Ortega_OLL", "Ortega_PBL"]
+    for algSet in two:
         print(algSet)
-        state_gen = ThreeStates.StateGenerator(
-            f"input/{algSet}.txt", f"states/{algSet}.json", algSets[algSet])
+        state_gen = TwoStates.StateGenerator(algSet)
         state_gen.writeStates()
 
-        scram_gen = scrambler.ScrambleGenerator(
-            f"states/{algSet}.json", f"scrambles/{algSet}.json")
+        scram_gen = TwoScrambles.ScrambleGenerator(algSet)
+        scram_gen.writeScrambles()
+
+    for algSet in three:
+        print(algSet)
+        state_gen = ThreeStates.StateGenerator(algSet, three[algSet])
+        state_gen.writeStates()
+
+        scram_gen = ThreeScrambles.ScrambleGenerator(algSet)
         scram_gen.writeScrambles()
 
 
